cdacabf10d178b77ef8e36cb6eb6d9425a123069
nukiska <g.nina@seznam.cz>
2022-12-05 22:25:10 +0100

    dictionary nahrazen listem

--- 2022_AoC/2022_day_01/2022_day_01.py
+++ 2022_AoC/2022_day_01/2022_day_01.py
@@ -1,28 +1,22 @@
-elves = {}
+elves = []
 
 
 def get_total_calories_maximum():
     food_calories = 0
-    elf_num = 1
-
     with open('2022_day_01_input.txt', encoding='utf-8') as f:
         for line in f:
             if line.rstrip() != '':
                 food_calories += int(line.rstrip())
             else:
-                elves[elf_num] = food_calories
+                elves.append(food_calories)
                 food_calories = 0
-                elf_num += 1
-        elves[elf_num] = food_calories
-
-    elf_with_max_calories = max(elves, key=elves.get)
-    max_total_calories = elves[elf_with_max_calories]
-    return max_total_calories
+        elves.append(food_calories)
+    return max(elves)
 
 
 def get_3_max_calories_amount():
-    sorted_total_calories = sorted(elves, key=elves.get, reverse=True)[:3]
-    return sum(elves[i] for i in sorted_total_calories)
+    sorted_total_calories = sorted(elves, reverse=True)[:3]
+    return sum(i for i in sorted_total_calories)
 
 
 if __name__ == '__main__':
